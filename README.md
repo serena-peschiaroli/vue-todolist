805afc6c5cc21582f037e9665acaef5ac3769451
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-15 17:53:09 +0100

    Fix: removeTask function + button, add: done button, taskDone function

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ### ESERCIZIO:
 Implementare una todolist base. Ogni todo è rappresentato come una stringa all'interno dell'array.
 **MILESTONE 1**
-Stampare all'interno di una lista, un item per ogni todo.
+[x]Stampare all'interno di una lista, un item per ogni todo.
 **MILESTONE 2**
-Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+[x]Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 **MILESTONE 3**
-Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+[x]Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 **BONUS 1**
 Oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 **BONUS 2**
--- js/script.js
+++ js/script.js
@@ -1,26 +1,45 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
-createApp ({
+createApp({
     data() {
         return {
             // array vuoto
-            newTask : "",
-            tasks: ["task1",  "task2", "task3"],
-
+            newTask: "",
+            tasks: [
+                { text: "fare la spesa", done: false },
+                { text: "pagare la luce", done: false },
+                { text: "fare la lavatrice", done: false },
+                { text: "buttare la plastica", done: false },
+            ],
         };
     },
     // funzioni
     methods: {
         // per aggiungere nuovo todo
-        addTask () {
-            if (this.newTask.trim() !== " "){
-                this.tasks.push(this.newTask.trim());
+        addTask() {
+            // Controlla se il nuovo item != spazio vuoto
+            if (this.newTask.trim() !== "") {
+                // Aggiunge il nuovo task all'array
+                this.tasks.push({ text: this.newTask.trim(), done: false });
+                // Resetta il campo di input
                 this.newTask = "";
+                // Log per indicare l'aggiunta del nuovo task
+                console.log("Nuovo item aggiunto:", this.tasks);
             }
         },
         // per rimuovere item
-        removeTask (index){
+        removeTask(index) {
+            // Rimuove iitem all'i specificato
             this.tasks.splice(index, 1);
-        }
-    }
-}).mount('#app');
+            // Log per  la rimozione di item
+            console.log("item rimosso:", this.tasks);
+        },
+        // per contrassegnare l'item come done
+        taskDone(index) {
+            // Inverte lo stato di "done" per il task all'i
+            this.tasks[index].done = !this.tasks[index].done;
+            // Log per cambiamento stato 
+            console.log("Stato item modificato:", this.tasks);
+        },
+    },
+}).mount("#app");
