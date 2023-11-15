6aed533d559b6163131ee136a3ec9f7e9155c5e3
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-15 15:57:46 +0100

    Add: button addTask

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@ const {createApp} = Vue;
 createApp ({
     data() {
         return {
+            // array vuoto
             newTask : "",
             tasks: ["task1",  "task2", "task3"],
 
+        };
+    },
+    // funzioni
+    methods: {
+        // per aggiungere nuovo todo
+        addTask () {
+            if (this.newTask.trim() !== " "){
+                this.tasks.push(this.newTask.trim());
+                this.newTask = "";
+            }
         }
     }
 }).mount('#app');
